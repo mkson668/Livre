2fe83c23b629e8f744ea556cf22a225c362c1d08
aaron <aaronwong668@gmail.com>
2020-07-06 22:46:50 -0700

    added entry screen

---
+++ src/screens/Bookcase.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// styling cannot be used from css btw
+// import './Bookcase.css';
+import {StyleSheet, Text, View} from 'react-native';
+
+class Bookcase extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Bookcase</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default Bookcase;
